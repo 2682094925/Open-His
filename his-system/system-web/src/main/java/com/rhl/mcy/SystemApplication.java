5c712e1eaa08b032d56c97ab8e62a640f155d9e8
small-live <2682094295@qq.com>
2022-07-15 18:25:15 +0800

    系统模块 基础构建

---
+++ his-system/system-web/src/main/java/com/rhl/mcy/SystemApplication.java
@@ -0,0 +1,22 @@
+package com.rhl.mcy;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@SpringBootApplication
+@MapperScan(basePackages = {"com.rhl.mcy.mapper"})
+@EnableDubbo
+public class SystemApplication {
+
+    public static void main(String[] args) {
+        Collections.sort(null,null);
+        SpringApplication.run(SystemApplication.class,args);
+    }
+}
+
+
